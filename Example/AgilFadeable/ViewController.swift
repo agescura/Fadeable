87c73fb6792c02677291310228ed187b6c198615
Albert Gil Escura <agilescura@gmail.com>
2016-08-27 01:17:21 +0200

    Implemented Fadeable and added example project

--- Example/AgilFadeable/ViewController.swift
+++ Example/AgilFadeable/ViewController.swift
@@ -7,17 +7,26 @@
 //
 
 import UIKit
+import AgilFadeable
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var box: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func fadeToggleTapped(sender: UIButton) {
+        if box.alpha == 0 {
+            box.fadeIn()
+        } else {
+            box.fadeOut()
+        }
     }
 
 }
