063a3592729f8bcdfe9295e1bf2d14e2ebc8ef4f
Albert Gil Escura <agilescura@gmail.com>
2016-08-27 00:59:43 +0200

    CocoaPods artifacts

---
+++ Example/Pods/Target Support Files/AgilFadeable/AgilFadeable-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AgilFadeable : NSObject
+@end
+@implementation PodsDummy_AgilFadeable
+@end
---
+++ Example/Pods/Target Support Files/AgilFadeable/AgilFadeable-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double AgilFadeableVersionNumber;
+FOUNDATION_EXPORT const unsigned char AgilFadeableVersionString[];
+
